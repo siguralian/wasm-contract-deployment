5cd0fc536023653506ce6a786b554ba6d5ae8414
siguralian <alianilyildirmaz@gmail.com>
2023-04-12 18:50:25 +0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -31,7 +31,6 @@ try {
   
   
   // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
   const seedUser = mnemonicToMiniSecret(PHRASE);
   const keyring = new Keyring({ type: "ed25519" });
   const userKeyring = keyring.addFromPair(naclKeypairFromSeed(seedUser));
